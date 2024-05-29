14597c6bba5ee8ce0664d9834cc0154058a0d5d6
Chris Robbins <crobbins@clearfocustech>
2024-05-29 20:22:33 +0000

    Changes per testing

--- destroy-lab.sh
+++ destroy-lab.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Really Confirm
 echo "Are you really sure you want to destroy the range ? Type the word YES to proceed, or anything else to cancel"
@@ -10,22 +10,22 @@ if [ "$confirm" != "YES" ]; then
 fi
 echo "Proceeding to destroy range in 3 seconds !! CTRL-C to cancel."
 sleep 1
-echo "Proceeding to destroy range in 1 seconds !! CTRL-C to cancel."
+echo "Proceeding to destroy range in 2 seconds !! CTRL-C to cancel."
 sleep 1
 echo "Proceeding to destroy range in 1 seconds !! CTRL-C to cancel."
 sleep 1
 #sudo apt-get install p7zip-full isomd5sum genisoimage python3-pyvmomi
 
-#echo "Enter your vsphere username, such as administrator@vsphere.local:"
-#prompt VSPHERE_USERNAME
+echo "Enter your vsphere username, such as administrator@vsphere.local:"
+read -rs VSPHERE_USERNAME
 #export VSPHERE_USERNAME
 
-#echo "Enter your vsphere password:"
-#prompt VSPHERE_PASSWORD
+echo "Enter your vsphere password:"
+read -rs VSPHERE_PASSWORD
 #export VSPHERE_PASSWORD
 
 #ln -s ./isos ./tasks/; ln -s ./templates ./tasks/
 
 
 ## This will remove every VM in the inventory
-ansible-playbook ./tasks/delete-vm-esxi.yml -l vsphere -i inventory.txt -b -k -K -u root --extra-vars "ansible_connection=local"
+ansible-playbook ./tasks/delete-vm-esxi.yml -l vsphere -i catfish-inventory.txt -b -k -K -u root --extra-vars "ansible_connection=local"
