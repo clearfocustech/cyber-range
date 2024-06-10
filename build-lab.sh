10dc497121de211b863a95bd7abbaa2e419b43ab
gdosey-cft <56988835+gdosey-cft@users.noreply.github.com>
2024-06-10 10:57:57 -0700

    Update build-lab.sh

--- build-lab.sh
+++ build-lab.sh
@@ -2,6 +2,8 @@
 
 #sudo apt-get install p7zip-full isomd5sum genisoimage python3-pyvmomi
 
+echo "If issues occur, you can add a ' -vvv' to the end of any ansible-playbook command in this script to help debug"
+
 echo "Enter your vsphere username, such as administrator@vsphere.local:"
 read -r VSPHERE_USERNAME
 export VSPHERE_USERNAME
@@ -10,10 +12,12 @@ echo "Enter your vsphere password:"
 read -rs VSPHERE_PASSWORD
 export VSPHERE_PASSWORD
 
+echo "Make sure you have linked the inventory.txt file, iso directory, and templates directory into this folder"
 #ln -s ./isos ./tasks/; ln -s ./templates ./tasks/
 
 
 ## Run this for one of each host with a unique iso
+echo "Making the Windows CD iso images bootable without prompt to continue"
 ansible-playbook ./tasks/make_win_cd_bootable.yml -l win10,win2019 -i catfish-inventory.txt -b -k -K -u root --extra-vars "ansible_connection=local"
 echo "If the above playbooks completed successfully, please go edit you inventory and update the iso_image variables to end with '-no-prompt.iso'"
 echo "Press any key to continue"
@@ -22,7 +26,9 @@ read var
 ## Then update the iso_image variable for each to the -no-prompt.iso name
 
 echo "Starting playbooks to install the hosts"
-ansible-playbook ./tasks/install-windows-esxi.yml -l win10,win2019 -i catfish-inventory.txt -b -k -K -u root --extra-vars "ansible_connection=local" -vvv
+echo "Starting Windows installation"
+ansible-playbook ./tasks/install-windows-esxi.yml -l win10,win2019 -i catfish-inventory.txt -b -k -K -u root --extra-vars "ansible_connection=local" 
+echo "Starting Linux installation"
 ansible-playbook ./tasks/install-ubuntu-esxi.yml -l ubuntu22,ubuntu20 -i catfish-inventory.txt -b -k -K -u root --extra-vars "ansible_connection=local"
 echo "Sleeping for 30 minutes to let windows finish install"
 sleep 1800
